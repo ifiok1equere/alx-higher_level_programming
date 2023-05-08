4134ec1018c37c1ae743881757533090bb6aa223
ifiok1equere <equereifiok1@gmail.com>
2023-05-08 23:57:26 +0100

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,17 +2,19 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *front;
+	listint_t *front, *back;
 
 	if (list == NULL)
 		return (0);
 
+	back = list;
 	front = list->next;
 
 	while (front != NULL)
 	{
-		if (front->next == list)
+		if (front->next == list || front->next == back)
 			return (1);
+		back = front;
 		front = front->next;
 	}
 	return (0);
