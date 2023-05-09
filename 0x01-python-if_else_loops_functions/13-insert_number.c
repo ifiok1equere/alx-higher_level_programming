884dfe3adef5ac4c135c3001ab92492dc1ff12e7
ifiok1equere <equereifiok1@gmail.com>
2023-05-10 00:36:53 +0100

    Interview question 2

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_node - inserts a new node in a sorted listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be inserted in sorted order in new node
+ *
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *current;
+
+	current = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (current->next != NULL)
+		{
+			if (current->n > number)
+				current = current->next;
+			else
+			{
+				new->next = current->next;
+				current->next = new;
+				break;
+			}
+		}
+	}
+
+	return (*head);
+}
