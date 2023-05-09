2ecfa1d846594c1c877ccf20e00786397000f835
ifiok1equere <equereifiok1@gmail.com>
2023-05-09 22:10:37 +0100

    task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_last_digit(number):
+    if number < 0:
+        num = ((number * -1) % 10)
+    else:
+        num = number % 10
+    print("{:d}".format(num), end="")
+    return(num)
