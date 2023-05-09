c0549394f532e7a79811f67dcfcab9bd44b8706b
ifiok1equere <equereifiok1@gmail.com>
2023-05-09 22:44:08 +0100

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    print("{}, {}, ".format(1, 2), end="")
+    for i in range(3, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("{:s} ".format("FizzBuzz"), end="")
+        elif i % 3 == 0:
+            print("{:s} ".format("Fizz"), end="")
+        elif i % 5 == 0:
+            print("{:s} ".format("Buzz"), end="")
+        else:
+            print("{:d} ".format(i), end="")
