e2e8457e2df5ed805115f988e04e80b0a24668f9
ifiok1equere <equereifiok1@gmail.com>
2023-05-11 12:31:21 +0100

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv)
+    if argc == 1:
+        print("{:d} arguments.".format(0))
+    elif argc == 2:
+        print("{:d} argument:".format(argc - 1))
+        print("{:d}: {:s}".format(argc -1, argv[argc - 1]))
+    else:
+        print("{:d} arguments:".format(argc - 1))
+        for i in range(1, argc):
+            print("{:d}: {:s}".format(i, argv[i]))
