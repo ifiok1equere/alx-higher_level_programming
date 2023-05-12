93d4cb3495781322ab8b0e5b2f52022b1c9f122d
ifiok1equere <equereifiok1@gmail.com>
2023-05-12 07:00:24 +0100

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.8
-import hidden_4
-for name in dir(hidden_4):
-    if name[0] != "_":
-        print(name)
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != "_":
+            print(name)
