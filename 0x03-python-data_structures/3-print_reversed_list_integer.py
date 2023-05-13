67e842026c47b96c0b7b71aececb7cea188e56cb
ifiok1equere <equereifiok1@gmail.com>
2023-05-13 08:03:33 +0100

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+  Prints all integers of a list, in reverse order.
 
+  Args:
+    my_list: A list of integers.
 
+  Returns:
+    None.
+  """
 def print_reversed_list_integer(my_list=[]):
     if my_list == []:
         return
