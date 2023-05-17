024380b70a84f965b423e08cfea6f8b5c4f5d377
ifiok1equere <equereifiok1@gmail.com>
2023-05-18 00:48:36 +0100

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,12 +2,10 @@
 
 
 def square_matrix_simple(matrix=[]):
-    if matrix == []:
-        return
     new_matrix = []
     for row in matrix:
         new_matrix.append(row[:])
-    for i in range(len(new_matrix)):
+    for i in range(len(matrix)):
         for j in range(len(new_matrix)):
             new_matrix[i][j] = matrix[i][j] ** 2
     return (new_matrix)
