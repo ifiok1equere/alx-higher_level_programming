227775fd5c193416c26a94cd90f59c8ce96647ea
ifiok1equere <equereifiok1@gmail.com>
2023-05-18 00:44:07 +0100

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,13 +2,10 @@
 
 
 def square_matrix_simple(matrix=[]):
-    if matrix is None:
-        return
     new_matrix = []
-    '#new_matrix = [row[:] for row in matrix]'
     for row in matrix:
         new_matrix.append(row[:])
-        for i in range(len(new_matrix)):
-            for j in range(len(new_matrix)):
-                new_matrix[i][j] *= new_matrix[i][j]
+    for i in range(len(new_matrix)):
+        for j in range(len(new_matrix)):
+            new_matrix[i][j] = matrix[i][j]**2
     return (new_matrix)
