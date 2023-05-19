96955aa84041c64923b742fa6c00f7fbc85d111f
ifiok1equere <equereifiok1@gmail.com>
2023-05-20 00:33:17 +0100

    [B[AInterview Question 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if roman_string is None or roman_string == "":
+        return 0
+    dict_1 = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if len(roman_string) == 1:
+        return dict_1[roman_string]
+    total = dict_1[roman_string[0]]
+    for i in range(1, len(roman_string)):
+        for key, value in dict_1.items():
+            if key == roman_string[i]:
+                if dict_1[key] <= dict_1[roman_string[i - 1]]:
+                    total += value
+                    break
+                else:
+                    total = total + dict_1[key] - (2 * dict_1[roman_string[i - 1]])
+                    break
+    return abs(total)
