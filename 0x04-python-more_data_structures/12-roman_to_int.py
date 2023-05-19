ec3bbf40ce196ad6473017196d7e5959adc978fe
ifiok1equere <equereifiok1@gmail.com>
2023-05-20 00:53:56 +0100

    Interview Question 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 
 def roman_to_int(roman_string):
-    if roman_string is None or roman_string == "":
+    if roman_string is None or type(roman_string) is not str:
         return 0
     dict_1 = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if len(roman_string) == 1:
