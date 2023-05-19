b767c933b5aa65c7371353f6c43adb87c89a124e
ifiok1equere <equereifiok1@gmail.com>
2023-05-19 20:20:35 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,9 +4,9 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    score = list(a_dictionary.values())
-    for key,value in a_dictionary.items():
-        for i in score:
-            if value > i:
-                best_scorer = key
-    return best_scorer
+    score = max(list(a_dictionary.values()))
+    for key, value in a_dictionary.items():
+            if value == score:
+                return key
+    else:
+        return None
