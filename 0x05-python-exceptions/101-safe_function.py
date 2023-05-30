a06a2984b58250abd7f1210696bdae0b82d29478
ifiok1equere <equereifiok1@gmail.com>
2023-05-30 01:36:58 +0100

    Advanced task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(args[0], args[1])
+        return result
+    except ZeroDivisionError as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
+    except IndexError as t:
+        print("Exception: ".format(t), file=sys.stderr)
+        return None
