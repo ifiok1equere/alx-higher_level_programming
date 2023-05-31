3b4ce51c459a6a4f251ac2179ea2fbaf9a6cf72d
ifiok1equere <equereifiok1@gmail.com>
2023-05-31 23:42:32 +0100

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,9 @@
-"""
-This is a module that provides a class definition for a square.
-"""
 #!/usr/bin/python3
+'''
+This is a module that provides a class definition for a square.
+'''
 
 
 class Square:
-    ''' 
-    The class defines a square.
-    '''
+    ''' The class defines a square. '''
     pass
