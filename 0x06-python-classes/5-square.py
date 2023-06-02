8ca407c5f42742beb18e5716106366978bf7739e
ifiok1equere <equereifiok1@gmail.com>
2023-06-02 13:08:34 +0100

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+'''
+This is a module that provides a class definition for a square.
+'''
+
+
+class Square:
+    ''' The class defines a square. '''
+    def __init__(self, size=0):
+        '''
+        The __init__ method creates a private attribute \
+        called size and is instantiated to a variable called size.
+
+        Args:
+            size (int): a critical parameter of a square
+        '''
+        self.size = size
+
+    def area(self):
+        '''
+        The area method is a public instance method that computes the area of a
+        square and returns the result.
+
+        Note:
+            Reference made to the private instance attribute size for function
+            argument.
+        '''
+        return self.__size * self.__size
+
+    def my_print(self):
+        '''
+        This method is a public instance method that prints to stdout the
+        square defined by size with the character '#'.
+
+        Note:
+            Reference made to the private instance attribute size for function
+            argument.
+        '''
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end="")
+                print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''
+        The setter method is a private instance method that ensures the size of
+        the square is set to the value size and cannot be changed.
+
+        Note:
+            Reference made to the private instance attribute size for function
+            argument.
+        '''
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
