540658c1336d249a27c62cfbf968a1d1b45c7ad2
ifiok1equere <equereifiok1@gmail.com>
2023-07-04 17:10:28 +0100

    Advanced task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""This module defines a class"""
+
+
 class LockedClass(object):
+    """ This class defines ground rules for
+    setting an attribute and raises errors when
+    violated """
+
     def __setattr__(self, attribute, value):
         if not attribute == "first_name":
             raise AttributeError(f"'{self.__class__.__name__}' \
