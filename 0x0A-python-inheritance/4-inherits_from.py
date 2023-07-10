bf81ce82c6e5fc33225291b21ed3df4ed9a0a99f
ifiok1equere <equereifiok1@gmail.com>
2023-07-10 22:42:21 +0100

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,7 +14,7 @@ def inherits_from(obj, a_class):
         True if it is an instance
     """
 
-    if issubclass(obj.__class__, a_class) and not type(obj) is a_class:
-        return True
+    if issubclass(obj.__class__, a_class) and (type(obj) is a_class):
+        return False
     else:
-        False
+        return True
