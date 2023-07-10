8f6990c6850ed4f1ea4d03ef6643b6a8ab75d85e
ifiok1equere <equereifiok1@gmail.com>
2023-07-10 19:40:00 +0100

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines a class inheritance"""
+
+
+class MyList(list):
+    """ This class inherits from list """
+
+    def print_sorted(self):
+        """This method prints a sorted list."""
+        
+        new_list = sorted(self)
+        print(new_list)
