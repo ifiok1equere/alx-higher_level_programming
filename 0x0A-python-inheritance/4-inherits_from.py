66ea530b2b9421ea1895aea8011c3a5a7ba5645d
ifiok1equere <equereifiok1@gmail.com>
2023-07-10 22:32:52 +0100

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines a function that operates on an object"""
+
+
+def inherits_from(obj, a_class):
+    """
+    This function checks the instance of an object.
+
+    Args:
+        obj: type(obj)
+        a_class: class
+
+    Return:
+        True if it is an instance
+    """
+
+    if issubclass(obj.__class__, a_class) and not type(obj) is a_class:
+        return True
+    else:
+        False
