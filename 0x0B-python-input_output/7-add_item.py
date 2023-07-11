a856011042f0272805a7752fff7b91cc255b0512
ifiok1equere <equereifiok1@gmail.com>
2023-07-12 00:14:30 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,8 +9,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-input_data = list(sys.argv[1:])
 
-save_to_json_file(input_data, filename)
-my_list = load_from_json_file(filename)
-print(my_list)
+args = sys.argv[1:]
+
+try:
+    existing_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    existing_list = []
+
+updated_list = existing_list + args
+save_to_json_file(updated_list, filename)
