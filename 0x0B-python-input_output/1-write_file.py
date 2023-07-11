970dd03acdc5c12e9c933d439d547efd1e8da01b
ifiok1equere <equereifiok1@gmail.com>
2023-07-11 19:45:39 +0100

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module creates a function that handles files"""
+
+
+def write_file(filename="", text=""):
+    """
+    This function reads a text file
+    
+    Endcoding: "UTF-8"
+
+    Args:
+        filename of type file
+
+    Return:
+        A pointer to the read file
+        
+    """
+
+    with open(filename, 'w') as f:
+        return f.write(text)
