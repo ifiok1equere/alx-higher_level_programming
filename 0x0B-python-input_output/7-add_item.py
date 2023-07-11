72e1f416308526f75ac9f9d764226a17de349dc0
ifiok1equere <equereifiok1@gmail.com>
2023-07-11 22:51:48 +0100

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module is a script that adds all 
+arguments to a Python list, and then save them to a file
+"""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+input_data = list(sys.argv[1:])
+
+save_to_json_file(input_data, filename)
+my_list = load_from_json_file(filename)
+print(my_list)
