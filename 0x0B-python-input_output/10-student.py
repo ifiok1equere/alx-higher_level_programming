349f72da2ff462b0674c454fd7c2be38dcb633c5
ifiok1equere <equereifiok1@gmail.com>
2023-07-12 05:05:48 +0100

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" My class module
+"""
+
+
+class Student:
+    """ My class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            attr = {}
+            list_dict = self.__dict__
+            for key, value in list_dict.items():
+                if key in attrs:
+                    attr[key] = value
+            return attr
+        else:
+            return self.__dict__
