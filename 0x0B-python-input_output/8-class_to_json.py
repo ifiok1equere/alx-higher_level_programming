c0737d4294a401876f847d59ee2a411d5111a219
ifiok1equere <equereifiok1@gmail.com>
2023-07-12 04:26:03 +0100

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module defines a function that works on an obj """
+
+
+def class_to_json(obj):
+    """
+    This function builds a dictionary from an object """
+    return obj.__dict__
