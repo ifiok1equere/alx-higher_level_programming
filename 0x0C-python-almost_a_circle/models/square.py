f3e91d1d68465144cf51324c8bc2fa6e8c1528b4
ifiok1equere <equereifiok1@gmail.com>
2023-07-19 20:46:00 +0100

    Square Class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,23 +10,34 @@ class Square(Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
+        """ This method initializes an instance of the square object """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """
+        The method is an overridding __str__
+        function for the square class
+        """
         return ("[Square] ({:d}) {:d}/{:d} - "
                 "{:d}".format(self.id, self.x, self.y,
                               self.height))
 
     @property
     def size(self):
+        """ This is the size getter method """
         return self.height
 
     @size.setter
     def size(self, value):
+        """ This is the size setter method """
         self.height = value
         self.width = value
 
     def update(self, *args, **kwargs):
+        """
+        This is a method that updates and
+        sets the attribute of a class instance
+        """
         attributes = ["id", "size", "x", "y"]
 
         if args:
@@ -39,4 +50,8 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """
+        This method return a dictionary
+        representation of a class instance
+        """
         return {"id": self.id, "x": self.x, "size": self.width, "y": self.y}
