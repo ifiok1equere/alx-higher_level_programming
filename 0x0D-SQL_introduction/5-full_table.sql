78fc204eb684a61724bc7a8d12d7b277c1216a72
ifiok1equere <equereifiok1@gmail.com>
2023-08-16 01:42:36 +0100

    task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 
 -- The database name will be passed as an argument of the mysql command
 -- You are not allowed to use the DESCRIBE or EXPLAIN statements
-DESC `first_table`;
+SHOW COLUMNS FROM `first_table`;
