a494a88f3942fd98062e039464d60786913a3906
ifiok1equere <equereifiok1@gmail.com>
2023-08-18 20:20:25 +0100

    Advanced task 20

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -2,4 +2,4 @@
 
 -- Write a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
 
-SELECT state, MAX(value) as max_temp FROM hbtn_0c_0.temperatures GROUP BY state ORDER BY state LIMIT 3;
+SELECT state, MAX(value) as max_temp FROM hbtn_0c_0.temperatures GROUP BY state;
