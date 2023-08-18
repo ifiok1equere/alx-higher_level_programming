047d94a923cce7cec873f760b06473c88bd98a3f
ifiok1equere <equereifiok1@gmail.com>
2023-08-18 19:08:54 +0100

    Advanced task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,10 +7,10 @@
 	-- Field name in first_table
 
 -- Convert the database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the first_table table to UTF8
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table COLLATE utf8mb4_unicode_ci;
 
 -- Convert the name field in the first_table table to UTF8
-ALTER TABLE hbtn_0c_0.first_table CHANGE name name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CHANGE name name VARCHAR(255) COLLATE utf8mb4_unicode_ci;
