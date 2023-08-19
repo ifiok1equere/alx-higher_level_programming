4520f7402ec75872378abefdf2f2e48e730e00d0
ifiok1equere <equereifiok1@gmail.com>
2023-08-19 19:36:04 +0100

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,6 +5,6 @@
 	-- If the database hbtn_0d_2 already exists, your script should not fail
 	-- If the user user_0d_2 already exists, your script should not failWrite a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
-CREATE DATABASE hbtn_0d_2 IF NOT EXISTS;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_0;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* IF EXISTS TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
