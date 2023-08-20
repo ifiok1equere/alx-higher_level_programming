0ef30efbced293d434d44f20ac3bb65efa21cc15
ifiok1equere <equereifiok1@gmail.com>
2023-08-20 02:47:51 +0100

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
 
-SELECT c.id, c.name, s.name  FROM cities as c JOIN states as s on c.state_id=s.id;
+SELECT cities.id, cities.name, states.name  FROM cities INNER JOIN states on cities.state_id=states.id;
