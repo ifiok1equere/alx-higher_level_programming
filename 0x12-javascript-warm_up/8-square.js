885e4c4f8d7532a4e1ee72a027a3a3409ee3a769
ifiok1equere <equereifiok1@gmail.com>
2023-09-11 15:55:43 +0100

    task 8: Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const process = require('process');
 const args = process.argv;
 
-if (args[2] !== 3 || isNaN(Number(args[2]))) {
+if (args.length !== 3 || isNaN(Number(args[2]))) {
   console.log('Missing size');
 } else {
   let i = 0;
