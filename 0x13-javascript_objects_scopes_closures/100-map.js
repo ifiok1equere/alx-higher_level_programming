ef864ab40bea9b1c377ddebb42dc7248bcc006bf
ifiok1equere <equereifiok1@gmail.com>
2023-09-13 16:09:48 +0100

    task 11: Advanced: Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
-const importList = require('./100-data');
+const importList = require('./100-data').list;
 
-const newList = importList.list.map((x) => x * importList.list.indexOf(x));
-console.log(importList.list);
+let i = -1;
+const newList = importList.map((x) => {
+  i++;
+  return x * i;
+});
+console.log(importList);
 console.log(newList);
