ff60632378ccb43cdedc3d5beb6eb5c28d7de897
ifiok1equere <equereifiok1@gmail.com>
2023-09-15 19:31:14 +0100

    task 1: Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,9 +8,13 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
     conn = MySQLdb.connect(
-            host="localhost", port=3306, user="ifiok",
-            passwd="1Fiok@equere", db="hbtn_0e_0_usa", charset="utf8"
+            host="localhost", port=3306, user=mysql_username,
+            passwd=mysql_password, db=database_name, charset="utf8"
             )
 
     cur = conn.cursor()
