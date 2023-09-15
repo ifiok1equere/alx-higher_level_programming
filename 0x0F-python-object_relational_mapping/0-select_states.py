aae397cd1835cafb5f8aadb2f942b30a0812ad45
ifiok1equere <equereifiok1@gmail.com>
2023-09-15 18:46:23 +0100

    task 0: Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,29 @@
-#!/usr/bin/python3 
-''' This module fetches all the states from the hbtn_0e_0_usa database
-kjjkladkkdl.rklrkalksfr
-ajfmlapw;f,a a;ermkfw;lr
-elkrf;awrf,llrv '''
+#!/usr/bin/python3
+'''This module fetches all the states from the hbtn_0e_0_usa database'''
 
 # -*- coding: utf-8
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-conn = MySQLdb.connect(
-        host="localhost", port=3306, user=mysql_username,
-        passwd=mysql_password, db=database_name, charset="utf8"
-                       )
+    conn = MySQLdb.connect(
+            host="localhost", port=3306, user=mysql_username,
+            passwd=mysql_password, db=database_name, charset="utf8"
+            )
 
-cur = conn.cursor()
+    cur = conn.cursor()
 
-# HERE I have to know SQL to grab all states in my database
-cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-query_rows = cur.fetchall()
+    query_rows = cur.fetchall()
 
-for row in query_rows:
-    print(row)
+    for row in query_rows:
+        print(row)
 
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()
