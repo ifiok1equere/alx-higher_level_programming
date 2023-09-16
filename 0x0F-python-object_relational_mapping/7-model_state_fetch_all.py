eeb0e192c03afba9cf7dfefd170c2e61e69c5b3f
ifiok1equere <equereifiok1@gmail.com>
2023-09-16 14:57:49 +0100

    task 7: SQLAlchemy: All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """This module lists all State objects from the hbtn_0e_6_usa database
 """
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
-import sys
 
 if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+    import sys
+
     user, paswd, db = sys.argv[1], sys.argv[2], sys.argv[3]
     conn_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(
             conn_str.format(user, paswd, db), pool_pre_ping=True
             )
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
