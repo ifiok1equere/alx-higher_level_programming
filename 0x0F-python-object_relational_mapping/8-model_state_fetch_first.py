9d2761f1ebefab4b774e80a939060c72dad7d20c
ifiok1equere <equereifiok1@gmail.com>
2023-09-16 16:06:39 +0100

    task 8: First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module lists all State objects from the hbtn_0e_6_usa database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+
+def first_state(user, paswd, db):
+    """This function prints the state in the states tabele of the
+    hbtn_0e_6_usa database
+    """
+
+    conn_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(
+            conn_str.format(user, paswd, db), pool_pre_ping=True
+            )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).first()
+
+    print('{}: {}'.format(states.id, states.name))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    user, paswd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    first_state(user, paswd, db)
