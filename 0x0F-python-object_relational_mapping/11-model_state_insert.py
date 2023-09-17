72df5cecfb4a0fd1bdb58d8ab3ae0398b33646c5
ifiok1equere <equereifiok1@gmail.com>
2023-09-17 13:06:05 +0100

    task 11: Add a new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module searches a State object from the hbtn_0e_6_usa database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+
+def add_state(user, paswd, db):
+    """This function print the id of a states that is searched for in the
+    states tabele of the hbtn_0e_6_usa database.
+    """
+
+    conn_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(
+            conn_str.format(user, paswd, db), pool_pre_ping=True
+            )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    user, paswd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    add_state(user, paswd, db)
