2f2ce733cf27f27488ff36bcd36a272077a30965
ifiok1equere <equereifiok1@gmail.com>
2023-11-14 23:36:43 +0100

    task 4: create table schema  in a database

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Create a table in the current database of my MySQL Server
-CREATE first_table IF NOT EXISTS(
-	"id", INT
-	"name", VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
 )
