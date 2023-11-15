9ffc3b096ecaa1893826cdb2e032d01be24744d8
ifiok1equere <equereifiok1@gmail.com>
2023-11-15 03:16:33 +0100

    task 19: top 3 cities temperature

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,2 @@
 -- Top 3 cities temperature query
-SELECT city, AVG(value) as avg_temp FROM temperatures WHERE MONTH(month) IN (7, 8) GROUP BY cit
-y ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp FROM hbtn_0c_0.temperatures WHERE month >= 7 AND month <= 8 GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
