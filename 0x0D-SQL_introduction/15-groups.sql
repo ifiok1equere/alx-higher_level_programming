d3114d8c3270ea5087eebff4f19e9c3a286d4886
ifiok1equere <equereifiok1@gmail.com>
2023-11-15 01:55:20 +0100

    task 15: Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Number of of records with the same score
-SELECT score, count(*) as number FROM second_table GROUP BY score;
+SELECT score, count(*) as number FROM second_table GROUP BY score ORDER BY score DESC;
