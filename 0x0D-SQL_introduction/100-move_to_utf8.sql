b3759ab73761d87db9575d8429b03b82dd371f53
ifiok1equere <equereifiok1@gmail.com>
2023-11-15 02:32:26 +0100

    task 17: advanced task: convert to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- Convert a database, table and column to UTF8(utf8mb4, collate utf8mb4_unicode_ci)
 USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY COLUMN id INT(11);
-ALTER TABLE first_table MODIFY COLUMN name TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name TEXT VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
