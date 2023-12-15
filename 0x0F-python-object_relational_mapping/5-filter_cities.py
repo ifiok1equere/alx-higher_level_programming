eca8198f72268cbb0135eaaf3a3a1486ea6c6caa
ifiok1equere <equereifiok1@gmail.com>
2023-12-15 09:25:14 +0100

    task 5: All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,21 +9,22 @@ if __name__ == "__main__":
             passwd=sys.argv[2], db=sys.argv[3],
             charset="utf8"
             )
+    
     cur = conn.cursor()
     state = sys.argv[4]
+    
     query = "SELECT cities.name FROM cities INNER JOIN states \
             ON cities.state_id = states.id WHERE states.name = '{}' \
             ORDER BY cities.id ASC".format(state)
+    
     cur.execute(query)
     query_rows = cur.fetchall()
+    
     result = ""
     for row in query_rows:
         result += ' '.join(map(str, row)) + ', '
     result = result[:-2]
     print(result)
-    '''
-    for row in query_rows:
-        print(row)
-    '''
+    
     cur.close()
     conn.close()
