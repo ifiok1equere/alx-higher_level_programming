115b2807abe8eb40cbf1f20a867570ba5da9380f
ifiok1equere <equereifiok1@gmail.com>
2023-12-15 11:04:07 +0100

    refactor code to meet requirement

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,6 +5,13 @@ of a State and an instance Base = declarative_base()
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+user = "root"
+pwrd = "October1990#124#"
+db = "hbtn_0e_6_usa"
+port = 3306
 
 Base = declarative_base()
 
@@ -13,3 +20,15 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+
+
+# create a core point for the db
+engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
+            user, pwrd, port, db
+            ),
+        pool_recycle=3600
+        )
+
+# create all the tables defined as Base and State classes
+Base.metadata.create_all(engine)
