caeb67b338a13b83a0563296d1e455f7cc921dee
ifiok1equere <equereifiok1@gmail.com>
2023-12-15 10:37:17 +0100

    task 6: add module documentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
+'''This module  contains the class definition
+of a State and an instance Base = declarative_base()
+'''
 
-# import sys
-# from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
-'''
-user = sys.argv[1]
-pwrd = sys.argv[2]
-db = sys.argv[3]
-port = 3306
-'''
 
 Base = declarative_base()
 
@@ -19,15 +13,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-
-'''
-engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
-            user, pwrd, port, db
-            ),
-        pool_pre_ping=True
-        )
-
-Base.metadata.create_all(engine)
-'''
