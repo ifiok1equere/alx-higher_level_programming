97a470f260da77e9cda7813240af81cbe710c215
ifiok1equere <equereifiok1@gmail.com>
2023-12-15 11:41:30 +0100

    task 6: meet other bd connection requirement

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,14 +5,19 @@ of a State and an instance Base = declarative_base()
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+user = "root"
+pwrd = "October1990#124#"
+db = "hbtn_0e_6_usa"
+port = 3306
 
 Base = declarative_base()
 
 
 class State(Base):
-    '''This is a class
-    definition of a State
-    object mapped to the states table
+    '''This is a class definition of a State
     '''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
@@ -20,3 +25,15 @@ class State(Base):
             String(128, charset='latin1', collation='latin1_general_ci'),
             nullable=False
             )
+
+
+# create a core point for the db
+engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
+            user, pwrd, port, db
+            ),
+        pool_recycle=3600, pool_pre_ping=True
+        )
+
+# create all the tables defined as Base and State classes
+Base.metadata.create_all(engine)
