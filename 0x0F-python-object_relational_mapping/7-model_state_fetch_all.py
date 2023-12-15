8017d46de42d61b15afd673cc9e644d15125e30c
ifiok1equere <equereifiok1@gmail.com>
2023-12-15 12:21:21 +0100

    task 7: add module documentation

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module  lists all State objects
+from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __main__ == "__main__":
+    # command line arguments for db credentials
+    user = sys.argv[1]
+    pwrd = sys.argv[2]
+    db = sys.argv[3]
+    port = 3306
+
+    # create a core point for the db
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
+                user, pwrd, port, db
+                ),
+            pool_recycle=3600
+            )
+    # create all the tables defined as Base and State classes
+    Base.metadata.create_all(engine)
+
+    # create Session to start talking to the db. This returns an object
+    Session = sessionmaker(bind=engine)
+
+    # instantiate a Session object
+    session = Session()
+
+    # query the db and print out information needed from the object returned
+    result = session.query(State.id, State.name)
+    for id, name in result:
+        print("{:d}: {:s}".format(id, name))
+
+    session.close()
