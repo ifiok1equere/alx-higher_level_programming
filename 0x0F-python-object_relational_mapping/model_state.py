fdd95434fb2efa6520a9001578b5cc8619c2aab7
ifiok1equere <equereifiok1@gmail.com>
2023-12-15 11:50:23 +0100

    task 6: add unique attribute to column

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,13 @@ of a State and an instance Base = declarative_base()
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+user = "root"
+pwrd = "October1990#124#"
+db = "hbtn_0e_6_usa"
+port = 3306
 
 Base = declarative_base()
 
@@ -22,3 +28,15 @@ class State(Base):
             String(128, charset='latin1', collation='latin1_general_ci'),
             nullable=False
             )
+
+
+# create a core point for the db
+engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
+            user, pwrd, port, db
+            ),
+        pool_recycle=3600, pool_pre_ping=True
+        )
+
+# create all the tables defined as Base and State classes
+Base.metadata.create_all(engine)
