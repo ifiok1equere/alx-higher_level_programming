9aba8a61c9957e6d543adff530bec023964ba40f
ifiok1equere <equereifiok1@gmail.com>
2023-12-15 12:11:43 +0100

    refactor code

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,6 +13,6 @@ class State(Base):
     '''
 
     __tablename__ = 'states'
-    id = Column(Integer(), unique=True, primary_key=True,
+    id = Column(Integer, unique=True, primary_key=True,
                 autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
