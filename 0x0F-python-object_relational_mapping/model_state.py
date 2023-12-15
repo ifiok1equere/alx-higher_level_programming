e598d52af5d237370b5fb7f3ffb157823f2733e3
ifiok1equere <equereifiok1@gmail.com>
2023-12-15 11:52:39 +0100

    task 6: takes task warning into account

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -30,13 +30,9 @@ class State(Base):
             )
 
 
-# create a core point for the db
 engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
             user, pwrd, port, db
             ),
         pool_recycle=3600, pool_pre_ping=True
         )
-
-# create all the tables defined as Base and State classes
-Base.metadata.create_all(engine)
