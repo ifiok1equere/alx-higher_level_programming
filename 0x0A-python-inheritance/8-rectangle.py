e66d72b532b9dd9816d7f5a5af5828b2c7f71cd9
ifiok1equere <equereifiok1@gmail.com>
2024-04-15 23:41:33 +0100

    correct checker error

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,7 +8,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """This class inherits from BaseGeometry"""
 
-
     def __init__(self, width, height):
         """Initialization method"""
 
