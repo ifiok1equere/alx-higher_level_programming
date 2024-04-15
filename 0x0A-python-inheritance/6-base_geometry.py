f8a6b559e87edd43a510e92c0a006b6fdadf7cee
ifiok1equere <equereifiok1@gmail.com>
2024-04-15 20:19:01 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" This is an Empty Class"""
+
+
+class BaseGeometry():
+    """
+    Empty Class.
+    """
+
+    def area(self):
+        """Computes area of a polygon"""
+
+        raise Exception("area() is not implemented")
