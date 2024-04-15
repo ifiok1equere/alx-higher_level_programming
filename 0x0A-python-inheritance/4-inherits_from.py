60d40e5aff26421bb44fb76e070bfab6c3c7bce0
ifiok1equere <equereifiok1@gmail.com>
2024-04-15 19:53:44 +0100

    task 4: resolve checker failure a 2nd time

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -17,7 +17,7 @@ def inherits_from(obj, a_class):
         True or False
     """
 
-    if isinstance(obj, a_class) and type(obj) != a_class:
+    if issubclass(obj.__class__, a_class) and type(obj) != a_class:
         return True
     else:
         return False
