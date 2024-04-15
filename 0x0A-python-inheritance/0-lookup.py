33cd28a07ab9cbf8b001afd4ff3e0ca7f951ba78
ifiok1equere <equereifiok1@gmail.com>
2024-04-15 14:48:50 +0100

    still working to satisfy checker requirement by making script executable

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,4 +6,10 @@ attributes and methods of an object.
 
 
 def lookup(obj):
+    '''
+    This function returns
+    the attr and meth's of
+    an object passed herein
+    '''
+
     return(dir(obj))
