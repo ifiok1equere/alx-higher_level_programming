30001161111e3f908a39a398c519cdf987704f84
ifiok1equere <equereifiok1@gmail.com>
2024-04-16 00:03:53 +0100

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,9 +11,9 @@ class Square(Rectangle):
     def __init__(self, size):
         """Initialization method"""
 
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self._size = size
+        super().__init__(size, size)
 
     def area(self):
         """Computes area of the square"""
