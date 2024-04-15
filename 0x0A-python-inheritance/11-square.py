2d1f5fba147edaa631a1d4cda2809efc79ba96c2
ifiok1equere <equereifiok1@gmail.com>
2024-04-16 00:10:26 +0100

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This is a square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This square class inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialization method"""
+
+        self.integer_validator("size", size)
+        self._size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Computes area of the square"""
+
+        return self._size * self._size
+
+    def __str__(self):
+        """Unofficoal representation of the square class"""
+
+        return ("[Square] {:d}/{:d}".format(self._size, self._size))
