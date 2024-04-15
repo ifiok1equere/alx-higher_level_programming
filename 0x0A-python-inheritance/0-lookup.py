16411a192a0d56c778de868d433407a4711cd087
ifiok1equere <equereifiok1@gmail.com>
2024-04-15 15:22:56 +0100

    sorted checker issues

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 '''
 This function returns the list of available
 attributes and methods of an object.
@@ -6,10 +7,15 @@ attributes and methods of an object.
 
 
 def lookup(obj):
-    '''
-    This function returns
-    the attr and meth's of
-    an object passed herein
-    '''
+    """
+    This function defines an object obj.
+
+    Args:
+        obj: type(obj)
+
+    Return:
+        Attribute and methods of the object
+        instance
+    """
 
     return(dir(obj))
