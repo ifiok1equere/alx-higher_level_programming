1d5b147c49df71c5fa1736571ae2eef879b1c400
ifiok1equere <equereifiok1@gmail.com>
2024-04-16 22:44:10 +0100

    resolve checker failure

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """This script processes arguments from cmd"""
-import sys
+import sys, os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+filename = "./add_item.json"
 obj = []
 
-if len(sys.argv) == 1:
+if not os.path.isfile(filename):
     save_to_json_file(obj, filename)
 else:
     obj = load_from_json_file(filename)
