70ed74483456636dc0a9e6a788a14d7e6c14638b
ifiok1equere <equereifiok1@gmail.com>
2024-04-16 23:26:19 +0100

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a function"""
+import json
+
+def class_to_json(obj):
+    """This function serializes an object
+
+    Args:
+        obj: Python object to be serialized
+
+    Return:
+        dictionary description with simple data structure
+    """
+
+    return obj.__dict__
