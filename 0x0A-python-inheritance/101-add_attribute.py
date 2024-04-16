e0c00f04462ca36788c3831b458986ab277d40d7
ifiok1equere <equereifiok1@gmail.com>
2024-04-16 01:27:04 +0100

    resolve checker 10

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,8 @@
 def add_attribute(obj, name, value):
     """Function dtermines when an attribute can be added"""
 
-    if obj.__class__.__module__ == 'builtins':
+    # if obj.__class__.__module__ == 'builtins':
+    if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
     else:
         setattr(obj, name, value)
