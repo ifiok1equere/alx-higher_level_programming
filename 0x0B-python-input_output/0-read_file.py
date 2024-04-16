be7b32a8650ea8187e95a7ec1e106968af85940e
ifiok1equere <equereifiok1@gmail.com>
2024-04-16 19:01:02 +0100

    task 1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module reads a text file"""
+
+
+def read_file(filename=""):
+    """Function reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename: type(string)
+
+    Return:
+        None
+    """
+
+    with open(filename, encoding='utf-8') as f:
+        file = f.read()
+    print(file)
