a692281292b3c224555756e91eedce01f7263e10
ifiok1equere <equereifiok1@gmail.com>
2024-04-16 22:58:36 +0100

    resolve checker failure

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-obj = []
 
-if not os.path.isfile("./add_item.json"):
-    save_to_json_file(obj, filename)
-else:
-    obj = load_from_json_file(filename)
-    for i in range(1, len(sys.argv)):
-        obj.append(sys.argv[i])
-    save_to_json_file(obj, filename)
+try:
+    existing_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    existing_list = []
+
+for i in range(1, len(sys.argv)):
+    obj.append(sys.argv[i])
+save_to_json_file(obj, filename)
