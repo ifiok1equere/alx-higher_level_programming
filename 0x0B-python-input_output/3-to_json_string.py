137918ade0b7af4b1e000351f8fa5459db24840f
ifiok1equere <equereifiok1@gmail.com>
2024-04-16 19:35:11 +0100

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module serializes"""
+import json
+
+def to_json_string(my_obj):
+    """Function appends to a text file (UTF8)
+
+    Args:
+        my_obj: python object to be serialized
+
+    Return:
+        JSON representation of an object (string)
+    """
+
+    return json.dumps(my_obj)
