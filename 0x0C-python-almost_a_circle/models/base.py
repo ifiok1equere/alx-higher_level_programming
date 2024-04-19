b29672e746e3aaba4f4baea7847e753e847f2a43
ifiok1equere <equereifiok1@gmail.com>
2024-04-19 19:35:32 +0100

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""THis module defines a model blueprint"""
+
+
+class Base():
+    """This class define a blueprint for creating objects"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialization Method"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
