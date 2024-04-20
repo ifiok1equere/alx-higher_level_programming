932e61f094cb828e4575abf7a168a9428bf3203d
ifiok1equere <equereifiok1@gmail.com>
2024-04-20 16:13:19 +0100

    Task 8: Update the class Rectangle by adding the public method def update(self, *args): that assigns an argument to each attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,3 +110,17 @@ class Rectangle(Base):
         return "[{:s}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
                 cls_name, id_, x, y, width, height
                 )
+
+    def update(self, *args):
+        """This method updates the attributes of the ractangle class"""
+
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) == 5:
+            self.y = args[4]
