3b01790492e98fec5a88057ecb728249464b3599
ifiok1equere <equereifiok1@gmail.com>
2024-04-21 22:09:05 +0100

    Task 14: add public method that returns the dictionary representation of a Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -56,3 +56,13 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+
+        return {
+                "id": self.id,
+                "x": self.x,
+                "size": self.size,
+                "y": self.y
+                }
