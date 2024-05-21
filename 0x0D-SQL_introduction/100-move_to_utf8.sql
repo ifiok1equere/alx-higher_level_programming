edd067899a507002c56c024f670e8cfbaa0eb2dc
ifiok1equere <equereifiok1@gmail.com>
2024-05-21 22:05:27 +0100

    task 17: advanced

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,10 @@
--- alter a table
-
-USE hbtn_0c_0;  -- Set the database to use
+USE hbtn_0c_0;
 
 -- Convert the database schema to UTF-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the data in the 'first_table' to UTF-8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- Convert the table to UTF-8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the 'name' column to UTF-8 (optional)
-ALTER TABLE first_table MODIFY name CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- Convert the 'name' field to UTF-8 (optional)
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
