b20ead3154238e3638ee954f67e45112c11a455a
ifiok1equere <equereifiok1@gmail.com>
2024-05-22 23:24:30 +0100

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- create another mysql server user
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	name VARCHAR(256) NOT NULL
+)
