23f60f312c15b1671ded1fa258472252fc7c0f57
ifiok1equere <equereifiok1@gmail.com>
2024-05-23 22:10:59 +0100

    task 17: advanvced task

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- querying a db
+
+SELECT DISTINCT new_table.name 
+FROM (
+	SELECT *
+	FROM tv_show_genres AS sg 
+	LEFT JOIN tv_genres AS g ON sg.genre_id = g.id
+) AS new_table 
+LEFT JOIN tv_shows AS newest 
+ON new_table.show_id = newest.id 
+WHERE new_table.id NOT IN (6, 1, 2, 7, 8) 
+ORDER BY new_table.name ASC;
