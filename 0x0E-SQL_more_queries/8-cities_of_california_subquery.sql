fb59bfb55931c0e55b53aa0e3a8a4eb93ca17412
ifiok1equere <equereifiok1@gmail.com>
2024-05-23 01:03:19 +0100

    task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,10 @@
 -- querying hbtn_0d_usa
 
-SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = "california") ORDER BY citi
-es.id ASC;
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT states.id
+    FROM states
+    WHERE states.name = 'California'
+)
+ORDER BY cities.id ASC;
