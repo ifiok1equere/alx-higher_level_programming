ca508da57ffd94c4c2da83c0176b6abfd6278e29
ifiok1equere <equereifiok1@gmail.com>
2024-06-23 20:03:28 +0100

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """Connecting to the hbtn_0e_0_usa db using the MySQLdb module"""
+
 import sys
 import MySQLdb
 
-usr = sys.argv[1]
-pwd = sys.argv[2]
-db_name = sys.argv[3]
-
-conn = MySQLdb.connect(
-        host="localhost", port=3306, user=usr,
-        passwd=pwd, db=db_name, charset="utf8"
-        )
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    conn = MySQLdb.connect(
+            host="localhost", port=3306, user=usr,
+            passwd=pwd, db=db_name, charset="utf8"
+            )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
